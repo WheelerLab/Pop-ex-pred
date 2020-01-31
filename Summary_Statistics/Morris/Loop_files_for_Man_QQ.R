54bb39c39b5f75a4773b709be8ff71b41da5b611
egeoffroy <soccer2xtreme@gmail.com>
2020-01-31 15:04:30 -0600

    Create Loop_files_for_Man_QQ.R

---
+++ Summary_Statistics/Morris/Loop_files_for_Man_QQ.R
@@ -0,0 +1,18 @@
+
+library(dplyr)
+source("/home/egeoffroy/Summary_Statistics/scripts/Man_QQ.R")
+all_dir <- list.dirs("/home/egeoffroy/Summary_Statistics/Summary_Stats/Morris", full.names=FALSE)
+print(all_dir)
+setwd(paste("/home/egeoffroy/Summary_Statistics/Summary_Stats/Morris/"))
+all_files = list.files(pattern = ".csv", full.names = TRUE, recursive = TRUE) #recursive is usually true except for plotting SMultiXcan results
+      
+        
+Sum_Stat_Name <- "Morris" #change for each GWAS Summary Statistic
+for (file in all_files) {
+        print(file)
+        directory <- normalizePath(dirname(file))
+        print(directory) 
+        # read in the csv
+        make_plots(file, directory, sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(file)), sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(file)))
+}
+
