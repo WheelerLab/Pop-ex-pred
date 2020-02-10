46cf7cb04bdc276a85187d39c40cf1805b91dd17
egeoffroy <soccer2xtreme@gmail.com>
2020-02-10 16:07:38 -0600

    Update GTEX_heatmap.R

--- Summary_Statistics/Wojcik/GTEX_heatmap.R
+++ Summary_Statistics/Wojcik/GTEX_heatmap.R
@@ -22,7 +22,7 @@ for(files in all_files){
 }
 
 print(list_group)
-new_table <- data.frame(phenotype=character(), significant_hits=integer(), stringsAsFactors=FALSE)
+new_table <- data.frame(tissue=character(), phenotype=character(), significant_hits=integer(), stringsAsFactors=FALSE)
 
 for(i in 1:length(list_group)){
   file <- paste("/home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/", str_remove(list_group[i], "./"), sep = "")
@@ -30,10 +30,23 @@ for(i in 1:length(list_group)){
   S_Pred_file <- fread(file, header = T,  sep = ',')
   S_Pred_file <- subset(S_Pred_file, select=c("gene_name"))
   tissue <- strsplit(file, "/")
-  pheno <- tissue[[1]][6]
+  pheno <- tissue[[1]][7]
+  pheno <- gsub("_", ' ', pheno)
+  if(pheno == "Waist-hip50"){
+        pheno = "Waist-hip ratio Female"
+  }
+  if(pheno == "Waist-hip51"){
+        pheno = "Waist-hip ratio Male"
+  }
+  if(pheno == "Waist-hip52"){
+        pheno = "Waist-hip ratio"
+  }
+  pheno <- gsub('[[:digit:]]+', '', pheno)
   print(pheno)
-  tissue <- tissue[[1]][8]
-  print(tissue)
+  tissue <- tissue[[1]][10]
+  tissue <- str_remove(tissue, "GTEX")
+  tissue <- str_remove(tissue, "sig_genes.csv")
+  tissue <- str_replace_all(tissue, "_", " ")
   S_Pred_file$tissue <- rep(tissue, nrow(S_Pred_file))
   S_Pred_file$phenotype <- rep(pheno, nrow(S_Pred_file))
   S_Pred_file$significant_hits <- rep(nrow(S_Pred_file), nrow(S_Pred_file))
@@ -45,3 +58,6 @@ for(i in 1:length(list_group)){
 new_table <- new_table[order(significant_hits), ]
 head(new_table)
 new_table <- unique(new_table)
+ggplot(new_table, aes(tissue, phenotype, fill= significant_hits)) + 
+  geom_tile()
+ggsave("/home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/GTEX_v7_heatmap.png")
