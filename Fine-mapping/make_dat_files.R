e93b15012d141f2f0d4213bd2c9193470b40f41a
egeoffroy <soccer2xtreme@gmail.com>
2020-02-12 10:43:09 -0600

    Create make_dat_files.R

---
+++ Fine-mapping/make_dat_files.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(data.table)
+
+chr <- c(1:22)
+test_chr <- 1
+group_id <- "AFR"
+dosage <- fread(paste("/home/egeoffroy/AFR_Dosages/chr", test_chr, "_AFR.dosage.txt.gz", sep = ''), header = F, stringsAsFactors = FALSE)
+
+expression <- fread("/home/egeoffroy/AFR_expression_ens.txt", header = TRUE, stringsAsFactors = FALSE)
+print(head(expression))
+snps_in_file <- fread("/home/egeoffroy/new_PredDB/AFR_unfiltered_total_snps_in_model.txt", header = FALSE, stringsAsFactors = FALSE)
+#print(snps_in_file)
+for(ex in nrow(expression)){
+  genotype <- data.frame()
+  gene <- expression[ex, 1]
+ # names(gene) <- NULL
+  print(gene)
+  pheno <- expression[ex, -1]
+  header <- data.frame("pheno", gene, group_id, pheno)
+  names(header) <- NULL
+    print(header)
+  gene_snps <- snps_in_file %>% filter(V1 == gene$PROBE_ID)
+  print(gene_snps)
+  if(nrow(gene_snps) > 0){
+    print(dosage)
+    dosage <- dosage %>% filter(V2 == gene_snps$V2)
+    for(i in nrow(dosage)){
+      print(dosage)
+      dosage_info <- dosage[i, c(6:ncol(dosage))]
+      print(ncol(dosage_info))
+      genotypes <- data.frame("geno", paste("chr", test_chr, ".", dosage[i, 3], sep = ''), group_id, dosage_info)
+    }
+    genotype <- rbind(genotype, genotypes)
+    colnames(genotype) <- header
+  write.table(paste(gene, ".dat", sep = ''), header = T, quote = F)
+  }
+  
+}
