de677046cdeb90d05c42a5f51d41e5fe7cce44c0
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-09 10:22:33 -0500

    Update MESA_dapg_run.sh

--- Fine-mapping/MESA_dapg_run.sh
+++ Fine-mapping/MESA_dapg_run.sh
@@ -2,9 +2,9 @@
 estimate=$1 #/home/elyse/MESA_cis_eqtl/AFA_estimate.txt
 ld_file=$2
 n=$3 #233
-ssd=$4
+sst=$4
 
 # Preliminary code
-dap-g -d_est estimate -d_ld ld_file -d_n n 
+dap-g -d_est estimate -d_ld ld_file -d_n n -d_syy sst
 
 
