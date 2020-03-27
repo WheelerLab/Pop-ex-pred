cf391bcbada93f47578460492c5cef6df9359227
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-27 14:08:32 -0500

    Add files via upload

---
+++ Fine-mapping/Wojcik/run_fastenloc_Wojcik.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Diastolic_blood.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Diastolic_blood
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Systolic_blood.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Systolic_blood
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_HDL.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_HDL
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_LDL.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_LDL
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_C-reactive.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_C-reactive
+
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Smoking.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Smoking
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Coffee.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Coffee
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_End_renal.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_End-renal
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_QT_interval.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_QT_interval
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_QRS_duration.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_QRS_duration
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Glomerular.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Glomerular
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Fasting_insulin.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Fasting_insulin
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Fasting_glucose.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Fasting_glucose
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Mean_corp_hemo.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Mean_corp_hemo
+
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Hemoglobin.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Hemoglobin
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_PR.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_PR
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Platelet.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Platelet
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Waist-hip50.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Waist-hip50
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Waist-hip51.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Waist-hip51
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Waist-hip52.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Waist-hip52
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Hypertension.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Hypertension
+fastenloc -eqtl AFA_all1Mb_fastenloc.eqtl.annotation.vcf.gz -gwas AFA_Wojcik/AFA_Wojcik_Diabetes.gwas.pip.gz -prefix AFA_all1Mb_Wojcik_Diabetes
---
+++ Fine-mapping/Wojcik/run_pop_Wojcik.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash /home/elyse/Fine-mapping_Pipeline/db2fastenloc/run_Wojcik_files.sh AFA
+bash /home/elyse/Fine-mapping_Pipeline/db2fastenloc/run_Wojcik_files.sh CAU
+
