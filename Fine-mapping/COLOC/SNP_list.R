9b0c92c771ace3a0772e18f4a1f6338a0e7da606
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-30 09:26:38 -0500

    Update SNP_list.R

--- Fine-mapping/COLOC/SNP_list.R
+++ Fine-mapping/COLOC/SNP_list.R
@@ -6,4 +6,4 @@ pheno <- args[2]
 data <- fread(args[1], header=T, stringsAsFactors=F)
 cat(data$n[2])
 data <- unique(data$variant_id)
-write.table(data, paste("GWAS_SNPs", pheno, ".txt", sep = ''), quote=F, row.names=F, col.names=F)
+write.table(data, paste("GWAS_SNPs_", pheno, ".txt", sep = ''), quote=F, row.names=F, col.names=F)
