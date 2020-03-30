e271727b823f84c7859e25183d0731b91829807b
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-30 09:27:10 -0500

    Update make_coloc_files.R

--- Fine-mapping/COLOC/make_coloc_files.R
+++ Fine-mapping/COLOC/make_coloc_files.R
@@ -13,7 +13,7 @@ pops <- c("AFA", "CAU", "HIS", "AFHI", "ALL") #do combined pops later
 pops_sample_size <- c(233, 578, 352, 585, 1163) #R doesn't have dicts so we're doing it a slgihtly more ratchet way
 
 
-sig_gene_SNPs <- fread(paste("/home/elyse/GWAS_SNPs', phenos, ".txt", sep = ''), header = F) #so we don't run all the SNPs b/c it takes forever
+sig_gene_SNPs <- fread(paste("/home/elyse/GWAS_SNPs_', phenos, ".txt", sep = ''), header = F) #so we don't run all the SNPs b/c it takes forever
 sig_gene_SNPs <- sig_gene_SNPs$V1
 
 for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
