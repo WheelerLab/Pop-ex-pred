45fa2c255b785f88a856f9158a2c750de2ef7228
Elyse Geoffroy <egeoffroy@luc.edu>
2020-03-30 14:13:40 -0500

    Update make_coloc_files.R

--- Fine-mapping/COLOC/make_coloc_files.R
+++ Fine-mapping/COLOC/make_coloc_files.R
@@ -2,32 +2,41 @@ library(data.table)
 library(dplyr)
 library(R.utils)
 
+print(getwd())
+
 "%&%" = function(a,b) paste(a,b,sep="")
 args = commandArgs(trailingOnly=TRUE)
 phenofile <- args[1]
-phenos <- args[2]
-n <- args[3]
-population <- args[4]
+meqtlfile <- args[2]
+frqfile <- args[3]
+phenos <- args[4]
+n <- args[5]
+pop <- args[6]
+
 chrs <- c(1:22)
-if(population == 'MESA'){
-  pops <- c("AFA", "CAU", "HIS", "AFHI", "ALL") #do combined pops later
-  pops_sample_size <- c(233, 578, 352, 585, 1163) #R doesn't have dicts so we're doing it a slgihtly more ratchet way
+if(pop == 'AFA'){
+  pop_sample_size <- 233
 }
-if(population == 'HapMap'){
-  pops <- c("YRI")
-  pops_sample_size <- c(107)
+if(pop == 'CAU'){
+  pop_sample_size <- 578
+}
+if(pop == 'HIS'){
+  pop_sample_size <- 352
+}
+if(pop == 'AFHI'){
+  pop_sample_size <- 585
+}
+if(pop == 'ALL'){
+  pop_sample_size <- 1163
+}
+if(pop == 'YRI'){
+  pop_sample_size <- 107
 }
 
-sig_gene_SNPs <- fread(paste("/home/elyse/GWAS_SNPs_", phenos, ".txt", sep = ''), header = F) #so we don't run all the SNPs b/c it takes forever
+sig_gene_SNPs <- fread(paste("GWAS_SNPs_", phenos, ".txt", sep = ''), header = F) #so we don't run all the SNPs b/c it takes forever
 sig_gene_SNPs <- sig_gene_SNPs$V1
 
-for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
-  if(population == 'MESA'){
-        frq <- fread("/home/angela/px_his_chol/MESA_compare/" %&% pops[pop] %&% ".frq")
-  }
-  if(population == 'HapMap'){
-        frq <- fread("/home/elyse/YRI_plink.frq")
-  }
+  frq <- fread(frqfile)
   frq <- frq %>% dplyr::select(SNP, MAF)
 
   for(pheno in phenos){ #read in GEMMA output file
@@ -41,14 +50,12 @@ for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
     GWAS_write <- GEMMA_for_COLOC
     eQTL_write <- data.frame(gene_id = character(), variant_id = character(), maf = numeric(), pval_nominal = numeric(), slope = numeric(), slope_se = numeric(), stringsAsFactors = F)
 
-    for(chr in chrs){ #yes triple loops are ratchet
-      if(population == 'MESA'){
-             system("zcat -f /home/wheelerlab3/files_for_revisions_plosgen/meqtl_results/MESA/" %&% pops[pop] %&% "_Nk_10_PFs_chr" %&% chr %&% "pcs_3.meqtl.cis.* > /home/elyse/coloc/meQTL_MESA_input.txt") #fread doesn't seem to like wildcards so we're gonna do this the ugly way
-             meqtl <- fread("/home/elyse/coloc/meQTL_MESA_input.txt", nThread = 40) #read in matrix eQTL results
+    for(chr in chrs){ 
+      if(pop == 'AFA' || pop == 'AFHI' || pop == 'HIS' || pop == 'CAU' || pop == 'ALL'){
+         system("zcat -f /home/wheelerlab3/files_for_revisions_plosgen/meqtl_results/MESA/" %&% pop %&% "_Nk_10_PFs_chr" %&% chr %&% "pcs_3.meqtl.cis.* > /coloc/meQTL_" %&% pop %&% "_input.txt") #fread doesn't seem to like wildcards so we're gonna do this the ugly way
+         meqtlfile <- "/coloc/meQTL_" %&% pop %&% "_input.txt"
       }
-      if(population == 'HapMap'){
-             meqtl <- fread("/home/elyse/meqtl_YRI_input.txt", nThread = 40) #read in matrix eQTL results
-      }
+      meqtl <- fread(meqtlfile, nThread = 40) #read in matrix eQTL results
       meqtl$se <- meqtl$beta / meqtl$statistic #make your own standard error since it's not in the meQTL output
       meqtl$n_samples <- pops_sample_size[pop]
       meQTL_for_COLOC <- left_join(meqtl, frq, by = c("snps" = "SNP")) #add freq to COLOC input
@@ -56,7 +63,6 @@ for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
       colnames(meQTL_for_COLOC) <- c("gene_id", "variant_id", "maf", "pval_nominal", "slope", "slope_se")
       meQTL_for_COLOC <- meQTL_for_COLOC[complete.cases(meQTL_for_COLOC),]
 
-      #GWAS_write <- rbind(GWAS_write, GEMMA_for_COLOC_chr)
       eQTL_write <- rbind(eQTL_write, meQTL_for_COLOC)
     }
 
@@ -64,13 +70,12 @@ for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
     snps_in_all <- intersect(snps_in_both, sig_gene_SNPs)
     GWAS_write <- subset(GWAS_write, panel_variant_id %in% snps_in_all)
     eQTL_write <- subset(eQTL_write, variant_id %in% snps_in_all)
-    #GWAS_write <- GWAS_write[order(GWAS_write$gene_id),] #don't order a column that doesn't exist
     eQTL_write <- eQTL_write[order(eQTL_write$gene_id),]
 
-    fwrite(eQTL_write, "/coloc/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
-    gzip("/coloc/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", destname = "/coloc/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt.gz") #script may only take .gz values so can't hurt to be too careful
-    fwrite(GWAS_write, "/coloc/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
-    gzip("/coloc/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", "/coloc/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt.gz")
-    print("Completed with " %&% pops[pop] %&% ", for " %&% pheno %&% ".")
+    fwrite(eQTL_write, "/coloc/eQTL_" %&% pop %&% "_" %&% pheno %&% ".txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
+    gzip("/coloc/eQTL_" %&% pop %&% "_" %&% pheno %&% ".txt", destname = "/coloc/eQTL_" %&% pop %&% "_" %&% pheno %&% ".txt.gz") #script may only take .gz values so can't hurt to be too careful
+    fwrite(GWAS_write, "/coloc/GWAS_" %&% pop %&% "_" %&% pheno %&% ".txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
+    gzip("/coloc/GWAS_" %&% pop %&% "_" %&% pheno %&% ".txt", "/coloc/GWAS_" %&% pop %&% "_" %&% pheno %&% ".txt.gz")
+    print("Completed with " %&% pop %&% ", for " %&% pheno %&% ".")
   }
 }
