a4894dbdd0807d99985b1df88d36de4429f920b4
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-01 10:06:56 -0500

    Update 05_Check_dif_harmonised_vs_old.sh

--- Summary_Statistics/Wojcik/05_Check_dif_harmonised_vs_old.sh
+++ Summary_Statistics/Wojcik/05_Check_dif_harmonised_vs_old.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#This file runs S-PrediXcan to check if harmonising the data has an effect on the output of S-PrediXcan. 
+#We used White-blood cell count as the test file
+
 cd MetaXcan/software
 python2 ./MetaXcan.py --model_db_path /home/egeoffroy/MESA/ALL_imputed_10_peer_3_pcs__v2.db --covariance /home/egeoffroy/MESA/ALL_nested_cv_a
 ll_covariances_10_peer_3pcs.txt.gz --gwas_folder /home/egeoffroy/ --gwas_file_pattern "31217584-GCST008049-EFO_0004308-build37.f.tsv.gz" --snp_column variant_id --effect_allele_
 column effect_allele --non_effect_allele_column other_allele --beta_column beta --pvalue_column p_value --output_file /home/egeoffroy/Wojcik37_White_blood_ALL.csv
 
-#This file runs S-PrediXcan to check if harmonising the data has an effect on the output of S-PrediXcan. We used White-blood cell count as the test file
