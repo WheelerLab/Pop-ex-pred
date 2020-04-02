4816540df347407feb083114c919799a20d80a06
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-02 08:40:57 -0500

    Update 07_run_LocusCompare.R

--- Summary_Statistics/Wojcik/07_run_LocusCompare.R
+++ Summary_Statistics/Wojcik/07_run_LocusCompare.R
@@ -3,6 +3,6 @@ library(locuscomparer)
 args = commandArgs(trailingOnly=TRUE)
 pheno <- args[1]
 pop <- args[2]
-gwas_fn = system.file('extdata','gwas.tsv', package = 'locuscomparer')
-eqtl_fn = system.file('extdata','eqtl.tsv', package = 'locuscomparer')
+gwas_fn = args[3]
+eqtl_fn <- args[4]
 locuscompare(in_fn1 = gwas_fn, in_fn2 = eqtl_fn, title1 =  pheno %&% ' GWAS', title2 = pop %&% ' eQTL')
