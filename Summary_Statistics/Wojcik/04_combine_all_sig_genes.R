b54af5e6e3f6b28a16bd592888714c4c1daa323e
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-22 08:57:12 -0500

    Update 04_combine_all_sig_genes.R

--- Summary_Statistics/Wojcik/04_combine_all_sig_genes.R
+++ Summary_Statistics/Wojcik/04_combine_all_sig_genes.R
@@ -3,39 +3,45 @@ library(data.table)
 library(dplyr)
 library(stringr)
 setwd("/home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/")
-all_files <- list.files(pattern="AFA2_sig_genes.csv", recursive=TRUE, full.names=T)
-all_files2 <- list.files(pattern="ALL2_sig_genes.csv", recursive=TRUE, full.names=T)
-all_files3 <- list.files(pattern="AFHI2_sig_genes.csv", recursive=TRUE, full.names=T)
-all_files4 <- list.files(pattern="HIS2_sig_genes.csv", recursive=TRUE, full.names=T)
-all_files5 <- list.files(pattern="CAU2_sig_genes.csv", recursive=TRUE, full.names=T)
+all_files <- list.files(pattern="AFA_sig_genes.csv", recursive=TRUE, full.names=T)
+all_files2 <- list.files(pattern="ALL_sig_genes.csv", recursive=TRUE, full.names=T)
+all_files3 <- list.files(pattern="AFHI_sig_genes.csv", recursive=TRUE, full.names=T)
+all_files4 <- list.files(pattern="HIS_sig_genes.csv", recursive=TRUE, full.names=T)
+all_files5 <- list.files(pattern="CAU_sig_genes.csv", recursive=TRUE, full.names=T)
 
 all_files <- c(all_files, all_files2, all_files3, all_files4, all_files5)
 
-print(all_files)
+#print(all_files)
 output <- data.frame()
 
 for(file in all_files){
+  if(!str_detect(file, 'MESA') && !str_detect(file, 'HapMap') && !str_detect(file, 'old')){
+  print(file)
   sig_genes <- fread(file, header=T, stringsAsFactors=T)
-  if(str_detect(file, "HIS")){
+  sig_genes <- sig_genes[,-1]
+  sig_genes <- sig_genes[,-13]
+  if(str_detect(file, "/HIS")){
     model <- "HIS"
   }
-  if(str_detect(file, "AFA")){
+  if(str_detect(file, "/AFA")){
     model <- "AFA"
   }
-  if(str_detect(file, "AFHI")){
+  if(str_detect(file, "/AFHI")){
     model <- "AFHI"
   }
-  if(str_detect(file, "CAU")){
+  if(str_detect(file, "/CAU")){
     model <- "CAU"
   }
-  if(str_detect(file, "ALL")){
+  if(str_detect(file, "/ALL")){
     model <- "ALL"
   }
-  phenotype <- rep(str_split(file, '/')[[1]][2], nrow(sig_genes))
-  model <- rep(model, nrow(sig_genes))
-  sig_genes <- cbind(phenotype, model, sig_genes)
+  Phenotype <- rep(str_split(file, '/')[[1]][2], nrow(sig_genes))
+  Model <- rep(model, nrow(sig_genes))
+  sig_genes <- cbind(Phenotype, Model, sig_genes)
   output <- rbind(output, sig_genes, fill=T)
-  print(output)
+  #print(output)
 }
-write.table(output, "/home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/sig_genes_all_pheno.txt", quote = F)
-
+}
+print(output)
+print(unique(output$gene_name))
+write.table(unique(output), "/home/egeoffroy/Summary_Statistics/Summary_Stats/Wojcik/sig_genes_all_pheno.csv", quote = F, sep = ',', row.names=F)
