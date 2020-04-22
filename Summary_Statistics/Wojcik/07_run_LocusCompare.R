0242ecfaa8a28587d8e110e3e386d28157787df4
Elyse Geoffroy <egeoffroy@luc.edu>
2020-04-22 08:52:33 -0500

    Delete 07_run_LocusCompare.R

--- Summary_Statistics/Wojcik/07_run_LocusCompare.R
+++
@@ -1,8 +0,0 @@
-library(locuscomparer)
-"%&%" = function(a,b) paste(a,b,sep="")
-args = commandArgs(trailingOnly=TRUE)
-pheno <- args[1]
-pop <- args[2]
-gwas_fn = args[3]
-eqtl_fn <- args[4]
-locuscompare(in_fn1 = gwas_fn, in_fn2 = eqtl_fn, title1 =  pheno %&% ' GWAS', title2 = pop %&% ' eQTL')
