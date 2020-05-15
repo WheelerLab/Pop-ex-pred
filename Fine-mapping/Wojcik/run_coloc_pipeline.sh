be93bb1d52124d3af5a282833bcd4e93efe04390
Elyse Geoffroy <egeoffroy@luc.edu>
2020-05-15 09:06:05 -0500

    Create run_coloc_pipeline.sh

---
+++ Fine-mapping/Wojcik/run_coloc_pipeline.sh
@@ -0,0 +1,33 @@
+pop=$1
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Body_Mass_Index --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008025-EFO_0004340-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Chronic_kidney --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008026-EFO_0003884-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Smoking --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008027-EFO_0006525-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Coffee --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008028-EFO_0006782-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Diastolic_blood --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008029-EFO_0006336-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Glomerular --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008030-EFO_0005208-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id End-renal --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008031-EFO_0003884-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Fasting_glucose --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008032-EFO_0004465-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Fasting_insulin --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008033-EFO_0004466-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Hemoglobin --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008034-EFO_0004541-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id HDL --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008035-EFO_0004612-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Hypertension --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008036-EFO_0000537-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id LDL --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008036-EFO_0000537-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Mean_corp_hemo --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008038-EFO_0004528-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Platelet --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008039-EFO_0004309-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id PR --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008042-EFO_0004462-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id QT_interval --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008043-EFO_0004682-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Systolic_blood --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008044-EFO_0006335-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Total_cholesterol --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008045-EFO_0004574-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Triglyceride --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008046-EFO_0004530-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Diabetes --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008048-EFO_0001360-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id White_blood --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008049-EFO_0004308-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Waist-hip50 --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008050-EFO_0004343-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Waist-hip51 --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008051-EFO_0004343-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Waist-hip52 --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008052-EFO_0004343-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id Height --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008053-EFO_0004339-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id QRS_duration --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008054-EFO_0005055-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
+python3 /home/elyse/coloc/coloc_pipeline_main.py --pheno_id C-reactive --pop ${pop} --gwas_SS /home/elyse/Wojcik_build37/31217584-GCST008055-EFO_0004458-build37.f.tsv.gz --frq /home/angela/px_his_chol/MESA_compare/${pop}.frq
